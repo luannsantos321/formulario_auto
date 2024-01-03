28c782057a68bccdbf0fc78edc90d45ac1b4ec98
Luann Rodrigo de Souza Santos <94115637+luannsantos321@users.noreply.github.com>
2024-01-03 16:25:41 -0300

    Update main.py
    adicionado labelframe para personalizar a GUI

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
-from customtkinter import *
+import customtkinter
 import tkinter as tk
 from tkinter import ttk
 
+
+
+
+
 win = tk.Tk()
 win.title("Formulário Automatizado")
-#win.configure(bg='white')
+win.configure(bg='white')
 
+#Aparelhos
+lb_aparelho = tk.LabelFrame(win,text='Aparelhos',bg='white',fg='orange'
+                            , padx=10, pady=10,font='Verdana, 30')
+aparelho = ttk.Combobox(lb_aparelho)
 
-#Frames
-fr = tk.Frame(win, height=100, width=100,bg='white',borderwidth=0.5,relief=tk.SOLID)
-fr2 = tk.Frame(win, bg="white", height=100, width=100,borderwidth=0.5,relief=tk.SOLID)
-fr3 = tk.Frame(win, bg="white", height=100, width=100,borderwidth=0.5,relief=tk.SOLID)
-fr4 = tk.Frame(win, bg="white", height=100, width=100,borderwidth=0.5,relief=tk.SOLID)
-fr5 = tk.Frame(win, bg="white", height=100, width=100,borderwidth=0.5,relief=tk.SOLID)
+lb_aparelho.place(x=40,y=90)
+aparelho.pack()
 
+#modelos
+#
+#(x=55,y=110)
+lb_modelo = tk.LabelFrame(win,text='Modelos',bg='white',fg='orange'
+                            , padx=10, pady=10)
+modelo = ttk.Combobox(lb_modelo)
 
+lb_modelo.place(x=250,y=90)
+modelo.pack()
 
+#defeito
+lb_defeito = tk.LabelFrame(win,text='Defeito',bg='white',fg='orange'
+                            , padx=10, pady=10)
+defeito = ttk.Combobox(lb_defeito)
 
-fr.grid(row=0,column=0, padx=5, pady=5)
-fr2.grid(row=0,column=4,padx=1, pady=1)
-fr3.grid(row=2,column=1,padx=1, pady=1)
-fr4.grid(row=3,column=0,padx=1, pady=1)
-fr5.grid(row=3,column=2,padx=1, pady=1)
+lb_defeito.place(x=40,y=180)
+defeito.pack()
 
-#Combobox
-'''lb1 = tk.Label(fr, text="Aparelhos")
-aparelho = ttk.Combobox(fr, values='')
+#conserto
+lb_conserto = tk.LabelFrame(win,text='Conserto',bg='white',fg='orange'
+                            , padx=10, pady=10)
+conserto = ttk.Combobox(lb_conserto)
 
-lb2 = tk.Label(fr2, text="Modelo")
-modelo = ttk.Combobox(fr2, values='')
+lb_conserto.place(x=250,y=180)
+conserto.pack()
 
-lb3 = tk.Label(fr3, text="Defeito")
-defeito = ttk.Combobox(fr3, values='')
+#Destino
+lb_destino =tk.LabelFrame(win,text='Destino',bg='white',fg='orange'
+                            , padx=10, pady=10)
+destino = ttk.Combobox(lb_destino)
 
-lb4 = tk.Label(fr4, text="Conserto")
-conserto = ttk.Combobox(fr4, values='')
+lb_destino.place(x=140,y=250)
+destino.pack()
 
-lb5 = tk.Label(fr5, text="Destino")
-destino = ttk.Combobox(fr5, values='')
-
-lb1.grid(row=0, column=0)
-aparelho.grid(row=0, column=0)
-lb2.grid(row=0, column=0)
-modelo.grid(row=1, column=0)
-lb3.grid()
-defeito.grid(row=0, column=0)
-lb4.grid()
-conserto.grid(row=1, column=0)
-lb5.grid(row=0, column=0)
-destino.grid(row=1, column=0)'''
-
-#Botão
-bt = tk.Button(win, text="Clique aqui")
-
-bt.grid(row=4,columnspan=3,sticky='ews')
-
-win.geometry('450x400')
+#botão
+bt = customtkinter.CTkButton(text='GO!', width=450, corner_radius=0, 
+                             fg_color='orange',text_color='white')
+bt.pack(side='bottom')
+win.maxsize(450,450)
+win.geometry('450x450')
 win.mainloop()
+
